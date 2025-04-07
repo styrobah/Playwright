f0debdd582cbcdee4f951a4b820dd245e2a03d34
Diogo Teixeira <diogoantonio_77@hotmail.com>
2025-04-08 00:55:41 +0200

    allure-reports

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,12 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter : [['list'],
+                ['allure-playwright',{outputFolder: 'my-allure-results'}]],
+  //OR YOU CAN USE A LIST, DOT, JSON, JUNIT ETC
+  //reporter: 'list',
+  //HERES AN EXAMPLE OF JSON
+  // reporter: [['json', {outputFile: 'results.json'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
